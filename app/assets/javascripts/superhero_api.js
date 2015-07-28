338a69f37c6ed0bba60402a3ef943dbd89b470d8
Bruce <brucepark0@gmail.com>
2015-07-27 22:34:54 -0700

    resource promise

--- app/assets/javascripts/superhero_api.js
+++ app/assets/javascripts/superhero_api.js
@@ -2,25 +2,27 @@
 
 (function(angular) {
   function ApiAction($resource) {
-    return $resource('/api/', 
+    return $resource('/api/',
       { },
       { api_index: {
         method: "GET",
         isArray: false 
         }
       }
-    );
+                    );
   }
 
   function superheroCtr($scope, ApiAction) {
     $scope.superheroSubmit = function() {
-      console.log("hello");
      // ApiAction.create({}, { superhero_name: $scope.superheroName, age: $scope.superheroAge });
     };
     $scope.superheroes = ApiAction.api_index({}, {});
-    console.log($scope.superheroes);
+    $scope.superheroes.$promise.then(function(data) {
+      console.log(data);
+    }, function(data) {
+    });
   }
-  
+
   var superheroApp = angular.module('superheroApp', ['ngResource']);
   superheroApp.controller('superheroCtr', ['$scope', 'ApiAction', superheroCtr]);
   superheroApp.factory('ApiAction', ['$resource', ApiAction]);
