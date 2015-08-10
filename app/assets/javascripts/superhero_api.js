1f67ccd1e3a6d1e10c9a1b8b8755c15c81f55799
Bruce <brucepark0@gmail.com>
2015-08-10 07:50:30 -0700

    refactor with

--- app/assets/javascripts/superhero_api.js
+++ app/assets/javascripts/superhero_api.js
@@ -2,37 +2,18 @@
 
 (function(angular) {
 
-  function ApiAction($resource, ResourceParameters) {
-    return $resource(ResourceParameters.route,
-      { },
-      { api_index: {
-        method: ResourceParameters.method,
+  function ApiAction($resource, resourceParams) {
+    return $resource('/api/:Id',
+      { Id: "@Id" },
+      { 
+        update: {
+        method: "PUT",
         isArray: true 
         }
       });
 
-    return $resource(ResourceParameters.route,
-      { },
-      { create: {
-        method: ResourceParameters.method,
-        isArray: true
-        }
-      }
-                    );
   }
 
-  function ResourceParameters() {
-    var factory = {};
-    factory.method = '';
-    factory.route = '';
-    factory.SetMethod = function(method) {
-      factory.method = method;
-    }
-    factory.SetRoute = function(route) {
-      factory.route = route;
-    }
-    return factory;
-  }
 
   function superheroCtr($scope, ApiAction, ResourceParameters) {
     $scope.superheroSubmit = function() {
@@ -41,13 +22,9 @@
   //      ApiAction.create({}, { superhero_name: hero.superhero_name, age: hero.age }); 
       });
     };
-    var heroesResources = ResourceParameters;
-    heroesResources.SetRoute('/api/');
-    heroesResources.SetMethod("GET");
-    console.log(heroesResources.method);
-    var heroes = ApiAction.api_index({}, heroesResources);
-    
-  /*  $scope.superheroes = [];
+    var heroes = ApiAction.query();
+   console.log(heroes); 
+    $scope.superheroes = [];
     heroes.$promise.then(function(data) {
       angular.forEach(data, function(item) {
         $scope.superheroes.push(item);
@@ -55,7 +32,7 @@
     }, function(data) {
       //if error then...
     });
-*/
+
     $scope.appendSuperheroFields = function() {
       var i = $scope.superheroes.length + 1;
       $scope.superheroes.push({"id": i, age: "", superhero_name: "" })
@@ -64,8 +41,7 @@
   }
 
   var superheroApp = angular.module('superheroApp', ['ngResource']);
-  superheroApp.controller('superheroCtr', ['$scope', 'ApiAction', 'ResourceParameters', superheroCtr]);
-  superheroApp.factory('ResourceParameters', [ ResourceParameters]);
-  superheroApp.factory('ApiAction', ['$resource', 'ResourceParameters', ApiAction]);
+  superheroApp.controller('superheroCtr', ['$scope', 'ApiAction', superheroCtr]);
+  superheroApp.factory('ApiAction', ['$resource', ApiAction]);
 
 })(angular);
